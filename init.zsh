905fa40549383c8bf7080cb6b904735f9966f81e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:18:49 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,16 @@
+p6df::modules::bash::version() { echo "0.0.1" }
+p6df::modules::bash::deps()    { 
+	ModuleDeps=(
+	)
+}
+
+p6df::modules::bash::external::brew() {
+
+  brew install bash
+}
+
+p6df::modules::bash::init() {
+
+}
+
+p6df::modules::bash::init
